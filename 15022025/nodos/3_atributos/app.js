911bb1454bf92a2aa5a7f511c5526abffc7918ad
jlplozada <jose_lpaezl@soy.sena.edu.co>
2025-02-25 20:59:12 -0500

    actualizacion

--- 15022025/nodos/3_atributos/app.js
+++ 15022025/nodos/3_atributos/app.js
@@ -1,3 +1,4 @@
+const body=document.body;
 const formulario=document.querySelector(`#formulario`)
 const nombre=document.getElementById(`nombre`);
 const apellido=document.querySelector(`#apellido`);
@@ -5,11 +6,17 @@ const telefono=document.querySelector(`#telefono`);
 const documento=document.querySelector(`#documento`);
 const usuario=document.querySelector(`#usuario`);
 const contrasena=document.querySelector(`#contrasena`);
-const btn_validar = document.querySelector(`button`);
+const btn = document.querySelector(`button`);
+const ciudades=async()=>{
+    const data =await fetch(`data.json`);
+    const ciudades=await data.json();
+    console.log(data); 
+}
 
 const validar = (event)=>{
     // detenemos evento
     event.preventDefault();
+    // validamos que el nombre tenga datos
  if (nombre.value ==``) {
     alert(`el nombre es obligatorio`);
     nombre.focus();
@@ -17,5 +24,29 @@ const validar = (event)=>{
     
  }
 }
-   
-btn_validar.addEventListener(`click`,validar)
+const dblclick=()=>{
+    alert (`doble click`);
+};
+
+const mousedown=()=>{
+    alert(`el evento funciona cuando se presona cualquier boton sobre el elemento`)
+}
+//    const contextmenu=()=>{
+//     alert(`?`)
+//    }
+const keydown=(event)=>{
+    // alert(`presiono la tecla ${event.key}`);
+    console.log(event)
+}
+// evento de tecla de sus tres posisiones
+
+// btn.addEventListener(`click`,validar);
+// btn.removeEventListener(`click`,validar);
+// formulario.addEventListener(`contextmenu`,contextmenu);
+// body.addEventListener(`contextmenu`,contextmenu);
+// body.addEventListener(`dblclick`,dblclick);
+// body.addEventListener(`mousedown`,mousedown);
+nombre.addEventListener(`keydown`,keydown);
+// btn.addEventListener(`click`,validar);
+
+ciudades();
